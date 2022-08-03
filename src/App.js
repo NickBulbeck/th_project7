cfaaa758195dde3adb3e887d9b0d077c1bd1960f
NickBulbeck <26549198+NickBulbeck@users.noreply.github.com>
2022-08-03 23:06:42 +0100

    Added some hint notes

--- src/App.js
+++ src/App.js
@@ -13,3 +13,12 @@ function App() {
 }
 
 export default App;
+
+// So: App is the top=level component. It's set up as a function in its own file.
+// We need to set up separate files for each other component and export them in the same way; 
+// then require them in here as applicable.
+
+// map: React basics, section 3, vid 4 (iterating with map()).
+
+// The biggest likely pitfall with statefull components is binding the methods to the class instance.
+// See section 4, vid 6 (bind event handlers) for reminders.
--- src/index.js
+++ src/index.js
@@ -7,9 +7,17 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App>
+      
+    </App>
   </React.StrictMode>
 );
+// Not seen the 'createRoot' method before, but I can see what it's doing.
+// You only render one component, traditionally: the top-level component.
+// Index.js is evidently run by react-scripts when you start the app. It 
+// looks up index.html in the public folder which, again, is an opinionated
+// name, evidently.
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
